13251dbeac0c2d68f3bb70343adfec88a05c8f1d
ShreyasVH <shreyas.deve.1@gmail.com>
2025-04-10 19:19:21 +0530

    password match

--- flight_search/auth_manager/forms.py
+++ flight_search/auth_manager/forms.py
@@ -11,6 +11,11 @@ class SignUpForm(UserCreationForm):
         help_text="Required only if registering as a Flight Company user"
     )
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].help_text = ''
+        self.fields['email'].required = True
+
     class Meta:
         model = User
         fields = ('username', 'email', 'role', 'flight_company', 'password1', 'password2')
