060b43816ccb2e6d2e5718da12535712759cccc1
ShreyasVH <shreyas.deve.1@gmail.com>
2025-04-10 11:42:45 +0530

    dockerized app

--- flight_search/flight_search/settings.py
+++ flight_search/flight_search/settings.py
@@ -82,10 +82,10 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "flight_search",
-        "USER": "shreyas",
-        "PASSWORD": "password",
-        "HOST": "localhost",
-        "PORT": "1369",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "postgres",
+        "PORT": "5432",
     }
 }
 
